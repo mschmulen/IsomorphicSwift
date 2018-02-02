443dc0fa48c4280f4ac5000c672897c96e336465
Matt Schmulen <matt.schmulen@gmail.com>
2018-02-02 09:25:52 -0600

    init

--- Sources/IsomorphicSwift/IsoPart.swift
+++ Sources/IsomorphicSwift/IsoPart.swift
@@ -3,4 +3,10 @@ import Foundation
 public struct IsoPart : Codable {
 	public var name:String
 	public var description:String
+	
+	public init(name: String, description: String) {
+		self.name = name
+	    self.description = description
+	}
+	
 }
--- Sources/IsomorphicSwift/IsoYacht.swift
+++ Sources/IsomorphicSwift/IsoYacht.swift
@@ -3,4 +3,10 @@ import Foundation
 public struct IsoYacht : Codable {
 	public var name:String
 	public var year:Int
+	
+	public init(name: String, year: Int) {
+		self.name = name
+	    self.year = year
+	}
+		
 }
